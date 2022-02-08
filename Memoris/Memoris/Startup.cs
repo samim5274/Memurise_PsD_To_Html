64c0103689b7ad3e67add6bd3a71549875c9f752
Samim Hossen <banglarcele1122@gmail.com>
2022-02-08 18:29:20 +0600

    Up

---
+++ Memoris/Memoris/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Memoris.Startup))]
+namespace Memoris
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
